ff41854b0c09161a7c96945d33e43f81f295ae64
Sascha Böck <wi16b142@technikum-wien.at>
2018-01-22 09:33:11 +0100

    edit xaml

---
+++ Training2.1/Training2.1/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Training2._1.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
